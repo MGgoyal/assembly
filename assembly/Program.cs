dd84af9f597c65880c179f62df818563ef2179b2
tanvi goyal <tanvi goyal@DESKTOP-Q4I1B7T>
2023-09-03 11:46:43 +0530

    Add project files.

---
+++ assembly/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Tutlane
+{
+    class User
+    {
+        public string Name;
+        public string Location;
+        public int Age;
+        public void GetUserDetails()
+        {
+            Console.WriteLine("Name: {0}", Name);
+            Console.WriteLine("Location: {0}", Location);
+            Console.WriteLine("Age: {0}", Age);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User u = new User();
+            u.Name = "Suresh Dasari";
+            u.Location = "Hyderabad";
+            u.Age = 32;
+            u.GetUserDetails();
+            Console.WriteLine("\nPress Enter Key to Exit..");
+            Console.ReadLine();
+        }
+    }
+}
+
